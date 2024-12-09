49dc647278c1b5acc31b26c4fdbd7c4bb0cb4483
RT-Northeastern <rt4373a@nulondonstudents.org>
2024-12-09 16:24:33 +0000

    Add files via upload

---
+++ test_is_prime.py
@@ -0,0 +1,16 @@
+from main import is_prime
+
+def test_is_prime_happy():
+    assert is_prime(2) == True 
+    assert is_prime(3) == True 
+    assert is_prime(7) == True 
+    assert is_prime(13) == True 
+
+def test_is_prime_unhappy():
+    assert is_prime(1) == False
+    assert is_prime(4) == False
+    assert is_prime(9) == False
+    assert is_prime(16) == False
+
+
+
